b7f69a255286964c86d602e6709c853c985458b2
dgtuann112 <dangthanhtuan0112@gmail.com>
2024-01-29 18:26:39 +0700

    29 Loginproject 2

--- projectvcpmc/src/component/Login.js
+++ projectvcpmc/src/component/Login.js
@@ -1,19 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import logoImage from "../images/logo.jpg";
+import exampleImage from "../images/n.png";
+import "./Login.css";
 
-export default function LoginPage() {
+<style>.login_page{}</style>;
+const SelectExample = () => {
+  //   const [selectedOption, setSelectedOption] = useState("option1");
+
+  //   const handleSelectChange = (event) => {
+  //     setSelectedOption(event.target.value);
+  //   };
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
-    <div className="login_page">
-      <div className="login-form-container">
-        <h1 className="title">Đăng Nhập</h1>
-        <form>
-          <div>
-            <label htmlFor="firt-name" className="form-label">
-              Tên đăng nhập
-            </label>
-            <input type="text" placeholder="Email" />
+    <div className="select_page">
+      <label htmlFor="firt-name" className="form-select">
+        Việt Nam
+        <img className="vnimg" src={exampleImage} alt="Example" />
+      </label>
+
+      <div className="login_page">
+        <div className="login-form-container">
+          <img className="lgoimg" src={logoImage} alt="Logo" />
+          <h3 className="title">Đăng nhập</h3>
+          <form>
+            <div>
+              <label htmlFor="firt-name" className="form-label">
+                Tên đăng nhập
+              </label>
+              <input
+                id="firt-name"
+                className={isFocused ? "custom-input foucused" : "custom-input"} //form-control
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                type="text"
+                placeholder=""
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="conform-password" className="form-label">
+                Password
+              </label>
+
+              <input
+                id="conform-password"
+                className={isFocused ? "custom-input foucused" : "custom-input"} //form-control
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                type="password"
+                placeholder=""
+              />
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" for="flexCheckDefault">
+                Ghi nhớ đăng nhập
+              </label>
+            </div>
+
+            <button type="submit" className="submit-btn">
+              Đăng nhập
+            </button>
+          </form>
+          <div classNam="forgot-pass">
+            <a className="linkpage" href="/#">
+              Quên mật khẩu?
+            </a>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default SelectExample;
