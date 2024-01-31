5e50fd4c4816e310a8af775c401690858f2e188c
dgtuann112 <dangthanhtuan0112@gmail.com>
2024-02-01 00:53:37 +0700

    1 successpass

--- projectvcpmc/src/App.js
+++ projectvcpmc/src/App.js
@@ -2,13 +2,14 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import FogotPass from "./component/FogotPass";
-import SelectExample from "./component/Login";
+// import SelectExample from "./component/Login";
+import SuccessPass from "./component/SuccessPass";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SelectExample />} />
+        <Route path="/" element={<SuccessPass />} />
         <Route path="/fogotpass" element={<FogotPass />} />
       </Routes>
     </Router>
--- projectvcpmc/src/component/SuccessPass.js
+++ projectvcpmc/src/component/SuccessPass.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import logoImage from "../images/logo.jpg";
 import exampleImage from "../images/n.png";
-import "./FogotPass.css";
+import "./SuccessPass.css";
 
-const FogotPass = () => {
+const SuccessPass = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [email, setEmail] = useState("");
   const [isSubmitSuccessful, setIsSubmitSuccessful] = useState(false);
@@ -37,7 +37,7 @@ const FogotPass = () => {
 
       <div className="login_page">
         <div className="login-form-container">
-          <img className="lgoimg" src={logoImage} alt="Logo" />
+          <img className="lgoimgsucc" src={logoImage} alt="Logo" />
           <h3 className="title">Khôi phục mật khẩu</h3>
 
           {isSubmitSuccessful ? (
@@ -48,42 +48,14 @@ const FogotPass = () => {
             </div>
           ) : (
             <form onSubmit={handleSubmit}>
-              <p className="restoremail">
-                Vui lòng nhập địa chỉ email đã đăng ký để khôi phục mật khẩu
+              <p className="restoremailsucc">
+                Link khôi phục mật khẩu đã được gửi vào mail của bạn. Vui lòng
+                kiểm tra mail.
               </p>
-              <div>
-                <label htmlFor="firt-name" className="form-label">
-                  Email
-                </label>
-                <input
-                  id="firt-name"
-                  className={
-                    isFocused ? "custom-input focused" : "custom-input"
-                  }
-                  onFocus={handleFocus}
-                  onBlur={handleBlur}
-                  type="text"
-                  placeholder=""
-                  value={email}
-                  onChange={handleEmailChange}
-                />
-              </div>
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-
-                <label className="form-check-label" htmlFor="flexCheckDefault">
-                  Ghi nhớ đăng nhập
-                </label>
-              </div>
-
-              <button type="submit" className="submit-btn">
-                Xác nhận
-              </button>
+              <p className="restoremailsucc">
+                Click vào đường link được đính kèm trong mail để chuyển đến
+                trang đặt lại mật khẩu.
+              </p>
             </form>
           )}
 
@@ -98,4 +70,4 @@ const FogotPass = () => {
   );
 };
 
-export default FogotPass;
+export default SuccessPass;
