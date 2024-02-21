a39d6c0eb207f33a075504d95b038e0ded3ded6a
dgtuan0112 <121471215+dgtuan0112@users.noreply.github.com>
2024-02-22 00:40:56 +0700

    22/2 prototype

--- projectvcpmc/src/component/User.js
+++ projectvcpmc/src/component/User.js
@@ -3,7 +3,14 @@ import { NavLink } from "react-router-dom";
 import logoImage from "../images/logo.jpg";
 import exampleImage from "../images/n.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import "./User.css";
@@ -23,38 +30,44 @@ const User = () => {
                 <ul class="navbar-nav flex-column ">
                   <li class="nav-item">
                     <img className="logo" src={logoImage} alt="Logo" />
-
-                    <a class="nav-link active" aria-current="page" href="/#">
-                      <FontAwesomeIcon icon="fa-solid fa-sliders" />
+                    <div className="prototype"></div>
+                    <FontAwesomeIcon icon={faSliders} />
+                    <a class="nav-link" href="/#">
                       Kho bản ghi
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faList} />
                     <a class="nav-link" href="/#">
                       Playlist
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faCalendarDays} />
                     <a class="nav-link" href="/#">
                       Lập lịch phát
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faFile} />
                     <a class="nav-link" href="/#">
                       Quản lý
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faDollarSign} />
                     <a class="nav-link" href="/#">
                       Doanh thu
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faGear} />
                     <a class="nav-link" href="/#">
                       Cài đặt
                     </a>
                   </li>
                   <li class="nav-item">
+                    <FontAwesomeIcon icon={faCircleQuestion} />
                     <a class="nav-link" href="/#">
                       Hỗ trợ
                     </a>
