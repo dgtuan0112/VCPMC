07413d9f01aef254cd473841250d1146d64f00f8
dgtuan0112 <121471215+dgtuan0112@users.noreply.github.com>
2024-03-04 02:09:06 +0700

    3/3

--- projectvcpmc/src/component/Manage.js
+++ projectvcpmc/src/component/Manage.js
@@ -21,93 +21,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Manage = () => {
   return (
     <div className="contract_page">
-      {/* <div className="select_user">
-        <label htmlFor="firt-name" className="form-select">
-          Việt Nam
-          <img className="vnimg" src={exampleImage} alt="Example" />
-        </label>
-      </div> */}
-
-      {/* <div className="row">
-      
-        <div className="col-2s">
-          {" "}
-          <ul class="navbar-nav flex-column ">
-            <li class="nav-item">
-              <img className="logo" src={logoImage} alt="Logo" />
-              <div className="prototype"></div>
-              <FontAwesomeIcon icon={faSliders} />
-              <a class="nav-link" href="/#">
-                Kho bản ghi
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faList} />
-              <a class="nav-link" href="/#">
-                Playlist
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faCalendarDays} />
-              <a class="nav-link" href="/#">
-                Lập lịch phát
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faFile} />
-              <a class="nav-link" href="/#">
-                Quản lý
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faDollarSign} />
-              <a class="nav-link" href="/#">
-                Doanh thu
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faGear} />
-              <a class="nav-link" href="/#">
-                Cài đặt
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faCircleQuestion} />
-              <a class="nav-link" href="/#">
-                Hỗ trợ
-              </a>
-            </li>
-          </ul>
-        </div>
-      
-
-        <div className="col-3">
-          <h4>Danh sách hợp đồng</h4>
-          <div className="col-1">
-            <div class="p-container">
-              <p className="mana1">Hợp đồng ủy quyền </p>
-              <p className="mana1">Hợp đồng ủy quyền </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-6">
-          <div className="row">sdaasdas</div>{" "}
-          
-        </div>
-
-        <div className="col-2 mag">
-          <ul class="navbar-nav flex-column ">
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faPlus} style={{ color: "#ff6600" }} />
-              <a class="nav-link" href="/#">
-                Thêm Hợp đồng
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div> */}
-
       <div className="Logo">
         <img className="logocontract" src={logoImage} alt="Logo" />
       </div>
@@ -119,7 +32,6 @@ const Manage = () => {
         </label>
       </div>
 
-      {/* <div className="container"> */}
       <div className="paging">
         <p className="paging1">quản lý - quản lý hợp đồng</p>
         <h4 className="paging2">Danh sách hợp đồng</h4>
@@ -234,7 +146,344 @@ const Manage = () => {
       </div>
       {/* </div> */}
       <div className="row">
-        <div className="col-9 frame"></div>
+        <div className="col-9 frame">
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>HĐ123</p>
+            </div>
+            <div className="col-2">
+              <p>hợp đồng ủy quyền bài hát</p>
+            </div>
+            <div className="col-2">
+              <p>Phan Mạnh Quỳnh</p>
+            </div>
+            <div className="col-2">
+              <p>Người biểu diễn</p>
+            </div>
+            <div className="col-2">
+              <p>Còn thời hạn</p>
+            </div>
+            <div className="col-1">
+              <p>14/2/2024</p>
+            </div>
+            <div className="col-1">
+              <div className="detail">
+                {" "}
+                <a class="nav-link" href="/#">
+                  xem chi tiết
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className=" footframe">
+              <div className="row">
+                {" "}
+                <div className="col-1 foot">
+                  <p>Hiển thị </p>
+                </div>
+                <div className="col-1 footip">
+                  {" "}
+                  <input
+                    type="text"
+                    className="form-control footer"
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                  />
+                </div>
+                <div className="col-8 foot">
+                  <p>hàng trong mỗi trang </p>
+                </div>
+                <div className="col-2 foot">
+                  <p>1 2 3 _100 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="col-2 contract ">
           <ul class="navbar-nav flex-column ">
             <li class="nav-item">
--- projectvcpmc/src/component/Record.js
+++ projectvcpmc/src/component/Record.js
@@ -6,7 +6,7 @@ import { faSliders } from "@fortawesome/free-solid-svg-icons";
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
-import "./Userr.css";
+import "./Record.css";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
@@ -20,99 +20,12 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Record = () => {
   return (
-    <div className="contract_page">
-      {/* <div className="select_user">
-        <label htmlFor="firt-name" className="form-select">
-          Việt Nam
-          <img className="vnimg" src={exampleImage} alt="Example" />
-        </label>
-      </div> */}
-
-      {/* <div className="row">
-      
-        <div className="col-2s">
-          {" "}
-          <ul class="navbar-nav flex-column ">
-            <li class="nav-item">
-              <img className="logo" src={logoImage} alt="Logo" />
-              <div className="prototype"></div>
-              <FontAwesomeIcon icon={faSliders} />
-              <a class="nav-link" href="/#">
-                Kho bản ghi
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faList} />
-              <a class="nav-link" href="/#">
-                Playlist
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faCalendarDays} />
-              <a class="nav-link" href="/#">
-                Lập lịch phát
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faFile} />
-              <a class="nav-link" href="/#">
-                Quản lý
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faDollarSign} />
-              <a class="nav-link" href="/#">
-                Doanh thu
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faGear} />
-              <a class="nav-link" href="/#">
-                Cài đặt
-              </a>
-            </li>
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faCircleQuestion} />
-              <a class="nav-link" href="/#">
-                Hỗ trợ
-              </a>
-            </li>
-          </ul>
-        </div>
-      
-
-        <div className="col-3">
-          <h4>Danh sách hợp đồng</h4>
-          <div className="col-1">
-            <div class="p-container">
-              <p className="mana1">Hợp đồng ủy quyền </p>
-              <p className="mana1">Hợp đồng ủy quyền </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-6">
-          <div className="row">sdaasdas</div>{" "}
-          
-        </div>
-
-        <div className="col-2 mag">
-          <ul class="navbar-nav flex-column ">
-            <li class="nav-item">
-              <FontAwesomeIcon icon={faPlus} style={{ color: "#ff6600" }} />
-              <a class="nav-link" href="/#">
-                Thêm Hợp đồng
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div> */}
-
+    <div className="record_page">
       <div className="Logo">
         <img className="logocontract" src={logoImage} alt="Logo" />
       </div>
 
-      <div className="select_contract">
+      <div className="select_record">
         <label htmlFor="firt-name" className="form-select ">
           Việt Nam
           <img className="vnimg" src={exampleImage} alt="Example" />
@@ -123,51 +36,6 @@ const Record = () => {
       <div className="paging">
         <h4 className="paging2">Kho bản ghi</h4>
         <div className="row tab">
-          <div className="col-1">
-            <p className="tab1">hợp đồng ủy quyền</p>
-          </div>
-
-          <div className="col-2">
-            <p className="tab2">hợp đồng ủy quyền</p>
-          </div>
-        </div>
-
-        <div className="row sele">
-          <div className="col-2">
-            <p className="labelsele1">Quyền sở hữu</p>
-          </div>
-          <div className="col-1">
-            <div className="sele1">
-              <select
-                id="seleone"
-                className="form-select"
-                aria-label="Default select example"
-              >
-                <option selected>tất cả</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
-          <div className="col-2">
-            <p className="labelsele2">Hiệu lực hợp đồng</p>
-          </div>
-
-          <div className="col-1">
-            <div className="sele2">
-              <select
-                id="seletwo"
-                className="form-select"
-                aria-label="Default select example"
-              >
-                <option selected>tất cả</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
           <div className="col-2">
             <input
               type="text"
@@ -179,6 +47,81 @@ const Record = () => {
           </div>
           <div className="col-2">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
+        <div className="col-11">
+          <div className="row selecor">
+            <div className="col-2">
+              <p className="labelselecor1">Thể loại</p>
+            </div>
+            <div className="col-1">
+              <div className="selecor1">
+                <select
+                  id="seleone"
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option selected>tất cả</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-2">
+              <p className="labelsele2">Định dạng</p>
+            </div>
+
+            <div className="col-1">
+              <div className="sele2">
+                <select
+                  id="selecortwo"
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option selected>tất cả</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-2">
+              <p className="labelselecor2">Thời hạn sử dụng</p>
+            </div>
+
+            <div className="col-1">
+              <div className="selecor2">
+                <select
+                  id="selecortwo"
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option selected>tất cả</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+            <div className="col-2">
+              <p className="labelselecor2">Trạng thái</p>
+            </div>
+
+            <div className="col-1">
+              <div className="selecor2">
+                <select
+                  id="selecortwo"
+                  className="form-select"
+                  aria-label="Default select example"
+                >
+                  <option selected>tất cả</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -233,7 +176,125 @@ const Record = () => {
       </div>
       {/* </div> */}
       <div className="row">
-        <div className="col-9 frame"></div>
+        <div className="col-9 frame">
+          <div className="row firtframe">
+            <div className="col-1">
+              <p>STT</p>
+            </div>
+            <div className="col-1">
+              <p>Số hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Tên hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Người ủy quyền</p>
+            </div>
+            <div className="col-1">
+              <p>Quyền sở hữu</p>
+            </div>
+            <div className="col-2">
+              <p>Hiệu lực hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>STT</p>
+            </div>
+            <div className="col-1">
+              <p>Số hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Tên hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Người ủy quyền</p>
+            </div>
+            <div className="col-1">
+              <p>Quyền sở hữu</p>
+            </div>
+            <div className="col-2">
+              <p>Hiệu lực hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>1</p>
+            </div>
+            <div className="col-1">
+              <p>Số hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Tên hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Người ủy quyền</p>
+            </div>
+            <div className="col-1">
+              <p>Quyền sở hữu</p>
+            </div>
+            <div className="col-2">
+              <p>Hiệu lực hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+          </div>
+          <div className="row contentframe">
+            <div className="col-1">
+              <p>STT</p>
+            </div>
+            <div className="col-1">
+              <p>Số hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Tên hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Người ủy quyền</p>
+            </div>
+            <div className="col-1">
+              <p>Quyền sở hữu</p>
+            </div>
+            <div className="col-2">
+              <p>Hiệu lực hợp đồng</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+            <div className="col-1">
+              <p>Ngày tạo</p>
+            </div>
+          </div>
+        </div>
+
         <div className="col-2 contract ">
           <ul class="navbar-nav flex-column ">
             <li class="nav-item">
